83d405ea608a7175f18fde6a74df544518d9c26a
Bhaswanth-K <Bhaswanth.K@encora.com>
2025-11-30 04:01:04 +0530

    everything working fine

--- src/main/java/com/capstone/experience/controller/ExperienceController.java
+++ src/main/java/com/capstone/experience/controller/ExperienceController.java
@@ -14,7 +14,7 @@ import java.util.List;
 
 @RestController
 @RequestMapping("/api/experience")
-@CrossOrigin(origins = "*")
+//@CrossOrigin(origins = "*")
 public class ExperienceController {
 
     private final ExperienceService experienceService;
